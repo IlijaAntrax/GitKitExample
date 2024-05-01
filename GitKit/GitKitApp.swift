faca82ff48c9a650e83c52cbf9d62c78cb96453a
Ilija Antonijevic <>
2024-05-01 13:17:19 +0200

    first init

---
+++ GitKit/GitKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitKitApp.swift
+//  GitKit
+//
+//  Created by Ilija Antonijevic on 1.5.24..
+//
+
+import SwiftUI
+
+@main
+struct GitKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
